8491c458b04c91a43b54980547a9459e7009beb8
delirium02 <darknightbs@gmail.com>
2026-05-14 06:24:11 +1000

    feat: receiveAttack function implemented

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -2,6 +2,7 @@ import Ship from "./ship";
 
 export function Gameboard() {
 	const board = [];
+	const ships = [];
 
 	for (let i = 0; i < 10; i++) {
 		const row = [];
@@ -46,6 +47,23 @@ export function Gameboard() {
 			for (let i = 0; i < length; i++) {
 				if (orientation === 'vertical') board[x + i][y] = ship;
 				if (orientation === 'horizontal') board[x][y + i] = ship;
+			}
+
+			ships.push(ship);
+		},
+
+		recieveAttack: function(x, y) {
+			if (x < 0 || x > 9 || y < 0 || y > 9) {
+				throw new RangeError("Cannot place ship: Coordinates must be between 0-9");
+			}
+
+			if (board[x][y] !== null) {
+				board[x][y].hit();
+				board[x][y] = "hit";
+				return "hit";
+			} else {
+				board[x][y] = "miss";
+				return "miss";
 			}
 		},
 	};

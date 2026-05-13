c8cae58bee9c649eb9f8da09068ac06f7c2cafe8
delirium02 <darknightbs@gmail.com>
2026-05-13 12:07:45 +1000

    refactor: adjusted placeShip function to be more concise

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -13,45 +13,40 @@ export function Gameboard() {
 	}
 
 	return {
-		placeShip: (ship, x, y, orientation) => {
-      const length = ship.getLength();
+		// Create an empty board
+		getBoard: () => board,
 
-      // safety checks for invalid placements
+		// Place ships
+		placeShip: (ship, x, y, orientation = 'vertical') => {
+			const length = ship.getLength();
+		
+			// Range checks
 			if (x < 0 || x > 9 || y < 0 || y > 9) {
-				throw new RangeError("Coordinates X and Y must be between 0-9");
+				throw new RangeError("Cannot place ship: Coordinates must be between 0-9");
 			}
-      for (let i = 0; i < length; i++) {
-        const targetRow = orientation === 'vertical' ? x + i : x;
-        const targetCol = orientation === 'horizontal' ? y + i : y;
 
-        if (board[targetRow][targetCol] !== null) {
-          throw new RangeError("Cannot place ship: Slot already occupied!");
-        }
-      }
+			if (orientation === 'vertical' && x + length >= 10) {
+				throw new RangeError("Out of bounds: Ship is too long for this column");
+			}
+			if (orientation === 'horizontal' && y + length >= 10) {
+				throw new RangeError("Out of bounds: Ship is too long for this row");
+			}
 
-      // find the location and add ship
-			if (orientation === "horizontal") {
-				if (y + length >= 10) {
-					throw new RangeError(
-						"Coordinate Y must remain between 0-9",
-					);
-				}
-				for (let i = 0; i < length; i++) {
-					board[x][y + i] = ship;
+			// Check the placement area is empty
+			for (let i = 0; i < length; i++) {
+				let checkX = orientation === 'vertical' ? x + i : x;
+				let checkY = orientation === 'horizontal' ? y + i: y;
+
+				if (board[checkX][checkY] !== null) {
+					throw new Error("Cannot place ship: Slot already occupied");
 				}
 			}
-			if (orientation === "vertical") {
-				if (x + length >= 10) {
-					throw new RangeError(
-						"Coordinate X must remain between 0-9",
-					);
-				}
-				for (let i = 0; i < length; i++) {
-					board[x + i][y] = ship;
-				}
+
+			// Place ship
+			for (let i = 0; i < length; i++) {
+				if (orientation === 'vertical') board[x + i][y] = ship;
+				if (orientation === 'horizontal') board[x][y + i] = ship;
 			}
 		},
-
-		getBoard: () => board,
 	};
 }
